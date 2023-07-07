aba47848869a189fb66ca92d92147c9399879f21
minwook <zkvn1103@naver.com>
2023-07-08 01:10:40 +0900

    스프링 빈 xml로 설정

---
+++ src/test/java/minwook/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package minwook.core.xml;
+
+import minwook.core.member.MemberService;
+import minwook.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
