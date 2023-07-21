84d2831611a9295cbd5141c0e6c3864b6eab2f2a
minwook <zkvn1103@naver.com>
2023-07-21 16:43:43 +0900

    옵션 처리

--- src/test/java/minwook/core/autowired/AutowiredTest.java
+++ src/test/java/minwook/core/autowired/AutowiredTest.java
@@ -19,17 +19,20 @@ public class AutowiredTest {
     static class TestBean {
 
         @Autowired(required = false)
+        // 자동 주입할 대상이 없으면 수정자 메서드 자체가 호출 안됨
         public void setNoBean1(Member noBean1) {
             System.out.println("noBean1 = " + noBean1);
         }
 
         @Autowired
         public void setNoBean2(@Nullable Member noBean2) {
+            // 자동 주입할 대상이 없으면 null이 입력됨 -> @Nullable
             System.out.println("noBean2 = " + noBean2);
         }
 
         @Autowired
         public void setNoBean3(Optional<Member> noBean3) {
+            // 자동 주입할 대상이 없으면 Optional.empty가 입력됨
             System.out.println("noBean3 = " + noBean3);
         }
     }
