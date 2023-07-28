dda96fe8a458030f296f8d0e2717f4ae79838553
minwook <zkvn1103@naver.com>
2023-07-28 15:00:49 +0900

    중복 빈 스캔 시 Primary 사용

--- src/main/java/minwook/core/discount/RateDiscountPolicy.java
+++ src/main/java/minwook/core/discount/RateDiscountPolicy.java
@@ -3,10 +3,11 @@ package minwook.core.discount;
 import minwook.core.member.Grade;
 import minwook.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
