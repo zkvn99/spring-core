06ed74511c68ef36f36d1ecafc16ecb559da8ac6
minwook <zkvn1103@naver.com>
2023-07-29 19:28:07 +0900

    빈 생명주기 콜백 시작

---
+++ src/test/java/minwook/core/autowired/AllBeanTest.java
@@ -0,0 +1,53 @@
+package minwook.core.autowired;
+
+import minwook.core.AutoAppConfig;
+import minwook.core.discount.DiscountPolicy;
+import minwook.core.member.Grade;
+import minwook.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        @Autowired
+        DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policiesMap = " + policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+}
---
+++ src/test/java/minwook/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,40 @@
+package minwook.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+        // 스프링 빈 라이프 사이클 = 객체 생성 -> 의존관계 주입
+        // 예외 - 생성자 주입
+        // 초기화 작업은 의존관계 주입이 모두 완료되고 난 다음에 호출해야 함
+        // 스프링 빈에게 콜백 메서드를 통해서 초기화 시점을 알려주는 다양한 기능 제공
+        // 스프링 빈의 이벤트 라이프사이클
+        // 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입
+        // 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료 (싱글톤 예시)
+        // 초기화 콜백은 빈이 생성되고, 빈이 의존관계 주입이 완료된 후 호출
+        // 소멸전 콜백은 빈이 소멸되기 직전에 호출
+        // SRP -> 객체의 생성과 초기화를 분리
+        // 생성자는 필수 정보(파라미터)를 받고 메모리 할당 및 객체를 생성하는 책임
+        // 초기화는 생성된 값을 활용해서 외부 커넥션에 연결하는 등 무거운 동작
+    }
+}
